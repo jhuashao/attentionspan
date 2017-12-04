b43889d2bd4de61fe3ee5469284054b422f8d674
jhuashao <joshua.h.shao@gmail.com>
2017-12-04 00:28:34 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+//counter
+var count = 505;
+
+var counter = setInterval(timer, 10); //10 will  run it every 100th of a second
+
+function timer() {
+    if (count <= 0) {
+        clearInterval(counter);
+        return;
+    }
+
+    count--;
+    document.getElementById("timer").innerHTML = count / 100;
+}
+
