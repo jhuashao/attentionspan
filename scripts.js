f75652223ed94e9c4e6e3f95ddd7ec5f0b4a6f9d
jhuashao <joshua.h.shao@gmail.com>
2017-12-04 16:54:35 -0500

    test

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ var counter = setInterval(timer, 10); //10 will  run it every 100th of a second
 
 function timer() {
     if (count <= 0) {
-        // clearInterval(counter);
-        // return;
+        clearInterval(counter);
+        return;
     }
     count--;
     document.getElementById("timer").innerHTML = count / 100;
